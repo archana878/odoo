00526730e11ef8179652925ee284a223c5780257
Archana Menon <archanamenon45@gmail.com>
2025-06-18 18:28:22 +0530

    Initial commit: Added estate module files

---
+++ tutorials/estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate',
+    'version': '0.1',
+    'summary': 'Real estate management',
+    'description': 'Module to manage real estate properties',
+    'category': 'Sales',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'installable': True,
+    'application': True,
+}
