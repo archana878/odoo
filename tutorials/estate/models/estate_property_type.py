d885e1f0ba10f989da84769634ee084b8c1fcdd5
Archana Menon <archanamenon45@gmail.com>
2025-06-23 21:25:09 +0530

    update

--- tutorials/estate/models/estate_property_type.py
+++ tutorials/estate/models/estate_property_type.py
@@ -1,9 +1,8 @@
-from odoo import fields, models
+from odoo import models,fields
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Property Type"
 
     name = fields.Char(required=True)
-    from odoo import fields, models
 
